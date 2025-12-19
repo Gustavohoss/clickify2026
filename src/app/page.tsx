b9ae87778613cf6e4972490bf478fdded805a965
Gustavo Henrique <ghkueigamer120@gmail.com>
2025-12-19 22:58:55 +0000

    Cade?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { BGPattern } from '@/components/ui/bg-pattern';
 
 export default function BlankPage() {
   return (
-    <div className="bg-black text-white flex items-center justify-center min-h-screen relative">
+    <div className="text-white flex items-center justify-center min-h-screen relative">
       <BGPattern variant="grid" fill="hsl(var(--border))" />
       <Logo />
     </div>
